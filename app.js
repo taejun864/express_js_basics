1cf458888db1296841edb18dfa1f92c39cf309de
taejun864 <taejun864@gmail.com>
2024-05-13 18:07:21 +0900

    using ejs template

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-const expressHbs = require('express-handlebars');
+// const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.engine('hbs', expressHbs({ layoutDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs', }));
-app.set('view engine', 'hbs');
+// app.engine('hbs', expressHbs({ layoutDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs', }));
+// app.set('view engine', 'hbs');
 // app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
