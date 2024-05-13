e2d46d1bf3158fac4ddbcb6a976b163e13bd77c4
taejun864 <taejun864@gmail.com>
2024-05-13 14:42:10 +0900

    using pug template

--- app.js
+++ app.js
@@ -4,18 +4,22 @@ const path = require('path');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', 'page-not-found.html'));
+    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.render('404');
 });
 
 app.listen(8080);
