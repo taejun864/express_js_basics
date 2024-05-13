6349f481ddbd1a8c1968c7a3b7a03020b6b30652
taejun864 <taejun864@gmail.com>
2024-05-13 17:39:55 +0900

    using handlebars template

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
+const expressHbs = require('express-handlebars');
+
 const app = express();
 
-app.set('view engine', 'pug');
+app.engine('hbs', expressHbs({ layoutDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs', }));
+app.set('view engine', 'hbs');
+// app.set('view engine', 'pug');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
@@ -19,7 +23,7 @@ app.use(shopRoutes);
 
 app.use((req, res, next) => {
     // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-    res.render('404');
+    res.render('404', { pageTitle: 'Page not Found' });
 });
 
 app.listen(8080);
